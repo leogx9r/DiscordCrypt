87b948281f9b0f920d8b5b20da5b8b1040382ecc
leogx9r <leogatesx9r@protonmail.com>
2018-06-13 03:38:17 -0400

    Switched to Koblitz curves for ECDH 409 bit variants.
    Signed-off-by: leogx9r <leogatesx9r@protonmail.com>

--- build/discordCrypt.plugin.js
+++ build/discordCrypt.plugin.js
@@ -7184,6 +7184,7 @@ class discordCrypt {
 
     /**
      * @public
+     * @see http://www.secg.org/sec2-v2.pdf
      * @desc Generates a Elliptic-Curve Diffie-Hellman key pair.
      * @param {int} size The bit length of the desired key pair.
      *      This must be one of the supported lengths retrieved from getECDHBitSizes().
@@ -7205,7 +7206,7 @@ class discordCrypt {
                 groupName = 'secp384r1';
                 break;
             case 409:
-                groupName = 'sect409r1';
+                groupName = 'sect409k1';
                 break;
             case 521:
                 groupName = 'secp521r1';
