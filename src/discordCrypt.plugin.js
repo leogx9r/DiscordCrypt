69b5374886e7758c87ee1d299052719bc0e9f128
leogx9r <leogatesx9r@protonmail.com>
2018-06-23 16:27:01 -0400

    Update log documentation.
    Signed-off-by: leogx9r <leogatesx9r@protonmail.com>

--- src/discordCrypt.plugin.js
+++ src/discordCrypt.plugin.js
@@ -1654,7 +1654,10 @@ class discordCrypt {
      * log( 'This is printed in red.', 'error' );
      *
      * @example
-     * log( 'This is printed green.', 'success' );
+     * log( 'This is printed green.', 'trace' );
+     *
+     * @example
+     * log( 'This is printed green.', 'debug' );
      *
      */
     static log( message, method = "info" ) {
