0bb944f0139d8ebe42823a696e05c275b1d30b69
leogx9r <leogatesx9r@protonmail.com>
2018-09-18 12:28:50 -0400

    Fixed unescaped line breaks.
    Signed-off-by: leogx9r <leogatesx9r@protonmail.com>

--- build/discordCrypt.plugin.js
+++ build/discordCrypt.plugin.js
@@ -8,17 +8,14 @@
     var pathSelf = WScript.ScriptFullName;
     shell.Popup("It looks like you mistakenly tried to run me directly. (don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
     if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-        shell.Popup("I'm in the correct folder already.
-Just reload Discord with Ctrl+R.", 0, "I'm already installed", 0x40);
+        shell.Popup("I'm in the correct folder already.\nJust reload Discord with Ctrl+R.", 0, "I'm already installed", 0x40);
     } else if (!fs.FolderExists(pathPlugins)) {
-        shell.Popup("I can't find the BetterDiscord plugins folder.
-Are you sure it's even installed?", 0, "Can't install myself", 0x10);
+        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
     } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
         fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
         // Show the user where to put plugins in the future
         shell.Exec("explorer " + pathPlugins);
-        shell.Popup("I'm installed!
-Just reload Discord with Ctrl+R.", 0, "Successfully installed", 0x40);
+        shell.Popup("I'm installed!\nJust reload Discord with Ctrl+R.", 0, "Successfully installed", 0x40);
     }
     WScript.Quit();
 @else @*/
--- src/build.js
+++ src/build.js
@@ -34,14 +34,14 @@ const INSTALL_SCRIPT_HEAD =
     var pathSelf = WScript.ScriptFullName;
     shell.Popup("It looks like you mistakenly tried to run me directly. (don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
     if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-        shell.Popup("I'm in the correct folder already.\nJust reload Discord with Ctrl+R.", 0, "I'm already installed", 0x40);
+        shell.Popup("I'm in the correct folder already.\\nJust reload Discord with Ctrl+R.", 0, "I'm already installed", 0x40);
     } else if (!fs.FolderExists(pathPlugins)) {
-        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+        shell.Popup("I can't find the BetterDiscord plugins folder.\\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
     } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
         fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
         // Show the user where to put plugins in the future
         shell.Exec("explorer " + pathPlugins);
-        shell.Popup("I'm installed!\nJust reload Discord with Ctrl+R.", 0, "Successfully installed", 0x40);
+        shell.Popup("I'm installed!\\nJust reload Discord with Ctrl+R.", 0, "Successfully installed", 0x40);
     }
     WScript.Quit();
 @else @*/\n\n`;
