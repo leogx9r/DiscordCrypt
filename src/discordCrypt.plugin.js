d0dde12a9e3da2e1126c54da4fd1f726f357ed5c
leogx9r <leogatesx9r@protonmail.com>
2018-10-19 09:29:23 -0400

    Updated to version 2.1.2.
    Signed-off-by: leogx9r <leogatesx9r@protonmail.com>

--- src/discordCrypt.plugin.js
+++ src/discordCrypt.plugin.js
@@ -940,7 +940,7 @@ const discordCrypt = ( ( ) => {
          * @returns {string}
          */
         getVersion() {
-            return '2.1.1';
+            return '2.1.2';
         }
 
         /**
