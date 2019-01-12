9844ce29a8d0c48299771acca823ee66cb2920d9
Leonardo Gates <leogatesx9r@protonmail.com>
2019-01-12 06:04:33 +0000

    Updated to version v2.2.1.
    Signed-off-by: Leonardo Gates <leogatesx9r@protonmail.com>

--- src/discordCrypt.plugin.js
+++ src/discordCrypt.plugin.js
@@ -945,7 +945,7 @@ const discordCrypt = ( ( ) => {
          * @returns {string}
          */
         getVersion() {
-            return '2.2.0';
+            return '2.2.1';
         }
 
         /**
